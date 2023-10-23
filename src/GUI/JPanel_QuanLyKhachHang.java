66cfc6f31b4082691955bcf166a673d7d24eadfe
Nguyen Nga <nguyennga200x@gmail.com>
2023-10-23 16:37:19 +0700

    ccsd

--- src/GUI/JPanel_QuanLyKhachHang.java
+++ src/GUI/JPanel_QuanLyKhachHang.java
@@ -15,6 +15,11 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Insets;
 import java.awt.geom.RoundRectangle2D;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
 public class JPanel_QuanLyKhachHang extends JPanel {
 
@@ -28,6 +33,8 @@ public class JPanel_QuanLyKhachHang extends JPanel {
 	private String hexColor_Blue4 = "#DBE4EE";
 	private String hexColor_Orange = "#F17300";
 	private String hexColor_Red = "#E11F1F";
+	private String hexColor_Green = "#4BAC4D";
+	private JTable table_KhachHang;
 
 	/**
 	 * Rounded JPanel
@@ -89,23 +96,67 @@ public class JPanel_QuanLyKhachHang extends JPanel {
 
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 0, 1286, 672);
-		panel.setBackground(Color.WHITE);
+		panel.setBackground(Color.decode(hexColor_Blue1));
 //		panel.setBorder(new RoundedTransparentBorder(20, Color.decode(hexColor_Blue1), Color.WHITE, 1.0f));
 		add(panel);
 		panel.setLayout(null);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 0, 1286, 85);
-		panel.add(panel_1);
+		JPanel panel_Button = new JPanel();
+		panel_Button.setBackground(new Color(255, 255, 255));
+		panel_Button.setBounds(0, 0, 492, 85);
+		panel.add(panel_Button);
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(0, 90, 177, 582);
-		panel.add(panel_2);
+		JPanel panel_Loc = new JPanel();
+		panel_Loc.setBorder(new RoundedTransparentBorder(20, Color.decode(hexColor_Blue1), Color.WHITE, 1.0f));
+		panel_Loc.setBackground(Color.decode(hexColor_Blue1));
+		panel_Loc.setBounds(0, 90, 221, 582);
+		panel.add(panel_Loc);
 		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBounds(179, 90, 1107, 582);
-		panel.add(panel_3);
+		JPanel panel_Table = new JPanel();
+		panel_Table.setBorder(new RoundedTransparentBorder(20, Color.decode(hexColor_Blue1), Color.WHITE, 1.0f));
+		panel_Table.setBackground(Color.decode(hexColor_Blue1));
+		panel_Table.setBounds(225, 90, 1061, 582);
+		panel.add(panel_Table);
+		panel_Table.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 10, 1041, 562);
+		panel_Table.add(scrollPane);
+		
+		table_KhachHang = new JTable();
+		table_KhachHang.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"M\u00E3 kh\u00E1ch h\u00E0ng", "H\u1ECD t\u00EAn", "Gi\u1EDBi t\u00EDnh", "Ng\u00E0y sinh", "\u0110\u1ECBa ch\u1EC9", "S\u1ED1 \u0111i\u1EC7n tho\u1EA1i", "\u0110i\u1EC3m th\u01B0\u1EDFng", "Ghi ch\u00FA"
+			}
+		));
+		table_KhachHang.setFont(new Font("Segoe UI", Font.BOLD, 13));
+		scrollPane.setViewportView(table_KhachHang);
+		
+		JButton btnTimKiem = new JButton("Tìm kiếm");
+		btnTimKiem.setIcon(new ImageIcon(JPanel_QuanLyKhachHang.class.getResource("/icon/search.png")));
+		btnTimKiem.setBackground(Color.decode(hexColor_Blue3));
+		btnTimKiem.setForeground(Color.WHITE);
+		btnTimKiem.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		btnTimKiem.setBounds(496, 55, 125, 30);
+		panel.add(btnTimKiem);
+		
+		JButton btnThem = new JButton("Thêm");
+		btnThem.setIcon(new ImageIcon(JPanel_QuanLyKhachHang.class.getResource("/icon/add.png")));
+		btnThem.setForeground(Color.WHITE);
+		btnThem.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		btnThem.setBackground(Color.decode(hexColor_Green));
+		btnThem.setBounds(631, 55, 125, 30);
+		panel.add(btnThem);
+		
+		JButton btnXoa = new JButton("Xóa");
+		btnXoa.setIcon(new ImageIcon(JPanel_QuanLyKhachHang.class.getResource("/icon/trash.png")));
+		btnXoa.setForeground(Color.WHITE);
+		btnXoa.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		btnXoa.setBackground(Color.decode(hexColor_Red));
+		btnXoa.setBounds(766, 55, 125, 30);
+		panel.add(btnXoa);
 
 	}
-
 }
